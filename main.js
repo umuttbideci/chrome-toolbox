daf0541ccd86f5291dfaa710a0eebb0ec5258359
umuttbideci <umutbideci@aol.com>
2023-11-06 16:09:01 +0300

    main files

---
+++ main.js
@@ -0,0 +1,31 @@
+function Mail(){
+    window.open("https://mail.google.com/mail/u/0/#inbox");
+}
+
+function Clasroom(){
+    window.open("https://classroom.google.com/");
+}
+
+function Docs(){
+    window.open("https://docs.google.com/document/u/0/");
+}
+
+function Youtube(){
+    window.open("https://www.youtube.com/");
+}
+
+function Drive(){
+    window.open("https://drive.google.com/drive/home");
+}
+
+function Github(){
+    window.open("https://github.com/")
+}
+
+function Whatsapp(){
+    window.open("https://web.whatsapp.com/")
+}
+
+function Search(){
+    window.open("https://www.google.com/?authuser=0")
+}
